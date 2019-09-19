1ef2bd695f3f8eeb86c6f55ea145fb486dc8cb4c
James Galovic <jpgalovic@gmail.com>
2019-09-19 12:50:03 +1000

    Add: GVR View Initalisation Add: Cardboard Trigger Handling (screen tap), Logged to Console Add: Various Status Logs

--- DaydreamerSandbox/src/main/java/com/jpgalovic/daydreamer/Navigation.java
+++ DaydreamerSandbox/src/main/java/com/jpgalovic/daydreamer/Navigation.java
@@ -1,13 +1,8 @@
 package com.jpgalovic.daydreamer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import android.util.Log;
-import android.widget.GridView;
 
-import com.google.vr.sdk.audio.GvrAudioEngine;
 import com.google.vr.sdk.base.AndroidCompat;
 import com.google.vr.sdk.base.Eye;
 import com.google.vr.sdk.base.GvrActivity;
@@ -17,12 +12,45 @@ import com.google.vr.sdk.base.Viewport;
 
 import javax.microedition.khronos.egl.EGLConfig;
 
+/**
+ * A VR Navigation Activity
+ *
+ * <p>This activity presents the user a scene consisting of a room that contains a desk with an
+ * old school style computer and floating objects that represent the various mini games and
+ * demonstrations that are available to the user.</p>
+ *
+ * <p>Upon selecting the computer screen or one of the floating objects, the user is taken to the
+ * relevant activity, respectively the source repository using the devices default web browser or
+ * the given mini game or demonstration.</p>
+ */
 public class Navigation extends GvrActivity implements GvrView.StereoRenderer {
+    private static final String TAG = "NavigationActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation);
+        initializeGvrView();
+    }
+
+    public void initializeGvrView() {
+        setContentView(R.layout.common_ui);
+
+        GvrView gvrView = findViewById(R.id.gvr_view);
+        gvrView.setEGLConfigChooser(8,8,8,8,16,8);
+
+        gvrView.setRenderer(this);
+        gvrView.setTransitionViewEnabled(true);
+
+        // Enable Cardboard-trigger feedback.
+        gvrView.enableCardboardTriggerEmulation();
+
+        // Async reprojection decouples the app frame rate from the display frame rate,
+        // this allows more immersive interaction event when the clock rate is throttled.
+        if(gvrView.setAsyncReprojectionEnabled(true)) {
+            AndroidCompat.setSustainedPerformanceMode(this,true);
+        }
+
+        setGvrView(gvrView);
     }
 
     @Override
@@ -47,12 +75,21 @@ public class Navigation extends GvrActivity implements GvrView.StereoRenderer {
     }
 
     @Override
-    public void onSurfaceChanged(int i, int i1) {
+    public void onCardboardTrigger() {
+        Log.i(TAG, "onCardboardTrigger");
 
+        //TODO: Handle Object Detection and Activity Transitions.
+
+        super.onCardboardTrigger();
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+        Log.i(TAG, "onSurfaceChanged");
     }
 
     @Override
     public void onRendererShutdown() {
-
+        Log.i(TAG, "onRendererShutdown");
     }
 }
