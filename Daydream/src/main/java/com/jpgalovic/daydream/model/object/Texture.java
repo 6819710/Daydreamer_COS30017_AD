8d6cd862b86809ed47549d4a539d13d7c622f3f0
James Galovic <jpgalovic@gmail.com>
2019-10-01 16:49:47 +1000

    Add: Texture

---
+++ Daydream/src/main/java/com/jpgalovic/daydream/model/object/Texture.java
@@ -0,0 +1,40 @@
+package com.jpgalovic.daydream.model.object;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
+
+import java.io.IOException;
+
+public class Texture {
+    private final int[] texureID = new int[1];
+
+    /**
+     * Inialises the texture
+     *
+     * @param context Context for loading the texture file.
+     * @param texturePath Path to the image to use for the texture.
+     */
+    public Texture (Context context, String texturePath) throws IOException {
+        GLES20.glGenTextures(1, texureID, 0);
+        bind();
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR_MIPMAP_NEAREST);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+        Bitmap textureBitmap = BitmapFactory.decodeStream(context.getAssets().open(texturePath));
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, textureBitmap, 0);
+        textureBitmap.recycle();
+
+        GLES20.glGenerateMipmap(GLES20.GL_TEXTURE_2D);
+    }
+
+    /** binds the texture to GL_TEXTURE0. */
+    public void bind() {
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texureID[0]);
+    }
+}
