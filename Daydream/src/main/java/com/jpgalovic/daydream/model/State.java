87c917e126c6c174a8323bb64c34702a8ef926c0
James Galovic <jpgalovic@gmail.com>
2019-10-01 16:12:00 +1000

    Add: Abstract State Class. Remove: Inoprable Sample test.

---
+++ Daydream/src/main/java/com/jpgalovic/daydream/model/State.java
@@ -0,0 +1,59 @@
+package com.jpgalovic.daydream.model;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+public abstract class State {
+    protected static String TAG;
+
+    protected ArrayList<State> connected;
+
+    /**
+     * Default constructor.
+     */
+    public State() {
+        TAG = "State";
+    }
+
+    /**
+     * Basic constructor. Sets object TAG.
+     * @param tag
+     */
+    public State(String tag) {
+        this.TAG = tag;
+    }
+
+    /**
+     * Adds given state to array list of connected states.
+     * @param state connected state
+     */
+    public void addConnection(State state) {
+        connected.add(state);
+    }
+
+    /**
+     * Initialises state.
+     */
+    public abstract void init();
+
+    /**
+     * Processees User Inputs. TODO: Determine types of imput to expose to method.
+     */
+    public abstract void input();
+
+    /**
+     * Updates the state.
+     */
+    public abstract State upate();
+
+    /**
+     * Renders the view for a given perspective.
+     * @param perspective                       Perspective Array.
+     * @param view                              View Array.
+     * @param headView                          Head View Array.
+     * @param objectProgram                     Object Program ID.
+     * @param objectModelViewProjectionParam    Object Model View Projection Parameter.
+     */
+    public abstract void render(float[] perspective, float[] view, float[] headView, int objectProgram, int objectModelViewProjectionParam);
+}
