ef028b586e329f3c0a4bd924907dee1739857b14
James Galovic <jpgalovic@gmail.com>
2019-10-04 19:54:11 +1000

    Add: HighScores State

---
+++ Daydream/src/main/java/com/jpgalovic/daydream/model/state/HighScores.java
@@ -0,0 +1,35 @@
+package com.jpgalovic.daydream.model.state;
+
+import android.content.Context;
+
+import com.jpgalovic.daydream.model.State;
+
+public class HighScores extends State {
+    // Object Data
+    // TODO: Add Object Data Here
+
+    // State Data
+    // TODO: Add State Data Here
+
+
+    @Override
+    public void init(Context context, int objectPositionParam, int objectUVParam) {
+        // TODO: Initialise each Object and StateData.
+    }
+
+    @Override
+    public void input(float[] headView) {
+        // TODO: Process User Inputs.
+    }
+
+    @Override
+    public State update() {
+        // TODO: Process State Updates.
+        return this;
+    }
+
+    @Override
+    public void render(float[] perspective, float[] view, float[] headView, int objectProgram, int objectModelViewProjectionParam) {
+        // TODO: Render Scene
+    }
+}
