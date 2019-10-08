aa63cd1ec5fdf07ab86e95566287782fd08c3840
James Galovic <jpgalovic@gmail.com>
2019-10-08 14:21:14 +1100

    Revert "Add: Selector Data"
    This reverts commit 9f14a98b

--- Daydream/src/main/java/com/jpgalovic/daydream/model/object/compound/AlphaNumeric.java
+++ Daydream/src/main/java/com/jpgalovic/daydream/model/object/compound/AlphaNumeric.java
@@ -4,8 +4,6 @@ import com.jpgalovic.daydream.Data;
 import com.jpgalovic.daydream.model.object.Texture;
 import com.jpgalovic.daydream.model.object.drawable.TexturedMeshObject;
 
-import java.util.ArrayList;
-
 public class AlphaNumeric {
     private static final String TAG = "AlphaNumeric";
 
@@ -16,71 +14,14 @@ public class AlphaNumeric {
             'U', 'V', 'W', 'X', 'Y', 'Z',
     };
 
-    private ArrayList<TexturedMeshObject> object;
+    private TexturedMeshObject object;
 
-    private int index;
-
-    private boolean flag_load_alpha;
-    private boolean flag_load_numeric;
-
-    public AlphaNumeric(boolean flag_load_alpha, boolean flag_load_numeric, char alphaNumeric, float x, float y, float z, float pitch, float yaw, float roll) {
-        this.flag_load_alpha = flag_load_alpha;
-        this.flag_load_numeric = flag_load_numeric;
-
-        object = new ArrayList<>();
-
-        if(flag_load_numeric) {
-            for(int i = 0; i < 10; i++) {
-                object.add(new TexturedMeshObject(String.valueOf(this.alphaNumeric[i]), true, Data.selectorMeshes.get(i), new Texture[]{ Data.selectorTextures.get(i)}, x, y, z, pitch, yaw, roll));
-            }
-        }
-
-        if(flag_load_alpha){
-            for(int i = 10; i < 36; i++) {
-                object.add(new TexturedMeshObject(String.valueOf(this.alphaNumeric[i]), true, Data.selectorMeshes.get(i), new Texture[]{ Data.selectorTextures.get(i)}, x, y, z, pitch, yaw, roll));
-            }
-        }
-
-        if(!(flag_load_alpha || flag_load_numeric)) {
-            object.add(new TexturedMeshObject(String.valueOf(alphaNumeric), true, Data.selectorMeshes.get(getIndex(alphaNumeric)), new Texture[]{ Data.selectorTextures.get(getIndex(alphaNumeric))}, x, y, z, pitch, yaw, roll));
-        }
-
-        index = getIndex(alphaNumeric);
+    public AlphaNumeric(int index, float x, float y, float z, float pitch, float yaw, float roll) {
+        object = new TexturedMeshObject(String.valueOf(alphaNumeric[index]), true, Data.alphaNumericMeshes.get(index), new Texture[]{ Data.alphaNumericTextures.get(index)}, x, y, z, pitch, yaw, roll);
     }
 
-    public void next() {
-        index++;
-        if(index >= 10 && !flag_load_alpha) {
-            if(flag_load_numeric) {
-                index = 0;
-            }
-        } else if(index >=36) {
-            if(flag_load_numeric) {
-                index = 0;
-            } else if (flag_load_alpha) {
-                index = 10;
-            }
-        }
-
-    }
-
-    public void prev() {
-        index--;
-        if(index < 10 && !flag_load_numeric) {
-            if(flag_load_alpha) {
-                index = 35;
-            }
-        } else if(index < 0) {
-            if(flag_load_alpha) {
-                index = 35;
-            } else if (flag_load_numeric) {
-                index = 9;
-            }
-        }
-    }
-
-    public void setChar(char alphaNumeric) {
-        index = getIndex(alphaNumeric);
+    public AlphaNumeric(char alphaNumeric, float x, float y, float z, float pitch, float yaw, float roll) {
+        object = new TexturedMeshObject(String.valueOf(alphaNumeric), true, Data.alphaNumericMeshes.get(getIndex(alphaNumeric)), new Texture[]{ Data.alphaNumericTextures.get(getIndex(alphaNumeric))}, x, y, z, pitch, yaw, roll);
     }
 
     private int getIndex(char alphaNumeric) throws RuntimeException {
@@ -93,41 +34,26 @@ public class AlphaNumeric {
     }
 
     public void setPosition(float x, float y, float z) {
-        for(int i = 0; i < object.size(); i++) {
-            object.get(i).setPosition(x, y, z);
-        }
+        object.setPosition(x, y, z);
     }
 
     public void translate(float x, float y, float z) {
-        for(int i = 0; i < object.size(); i++) {
-            object.get(i).translate(x, y, z);
-        }
+        object.translate(x, y, z);
     }
 
     public void setRotation(float pitch, float yaw, float roll) {
-        for(int i = 0; i < object.size(); i++) {
-            object.get(i).setRotation(pitch, yaw, roll);
-        }
+        object.setRotation(pitch, yaw, roll);
     }
 
     public void rotate(float pitch, float yaw, float roll) {
-        for(int i = 0; i < object.size(); i++) {
-            object.get(i).rotate(pitch, yaw, roll);
-        }
+        object.rotate(pitch, yaw, roll);
     }
 
     public void render(float[] perspective, float[] view, int objectProgram, int objectModelViewProjectionParam) {
-        if(flag_load_numeric) {
-            object.get(index).render(perspective, view, 0, objectProgram, objectModelViewProjectionParam);
-        } else if (flag_load_alpha) {
-            object.get(index - 10).render(perspective, view, 0, objectProgram, objectModelViewProjectionParam);
-        } else {
-            object.get(0).render(perspective, view, 0, objectProgram, objectModelViewProjectionParam);
-        }
-
+        object.render(perspective, view, 0, objectProgram, objectModelViewProjectionParam);
     }
 
     public boolean isLookedAt(float[] headView) {
-        return object.get(0).isLookedAt(headView);
+        return object.isLookedAt(headView);
     }
 }
