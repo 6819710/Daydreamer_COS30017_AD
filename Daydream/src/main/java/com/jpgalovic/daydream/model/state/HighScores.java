f2a11d51e634ce74781409916c9d9f60d471c7cf
James Galovic <jpgalovic@gmail.com>
2019-10-25 00:26:13 +1100

    Change: High Scores state now uses a back button and displays the name of the game above the score table.

--- Daydream/src/main/java/com/jpgalovic/daydream/model/state/HighScores.java
+++ Daydream/src/main/java/com/jpgalovic/daydream/model/state/HighScores.java
@@ -1,10 +1,13 @@
 package com.jpgalovic.daydream.model.state;
 
 import android.content.Context;
+import android.view.ViewDebug;
 
+import com.jpgalovic.daydream.Data;
 import com.jpgalovic.daydream.R;
 import com.jpgalovic.daydream.model.State;
 import com.jpgalovic.daydream.model.object.compound.ScoreDisplay;
+import com.jpgalovic.daydream.model.object.drawable.TexturedMeshObject;
 import com.jpgalovic.daydream.model.score.ScoreManager;
 import com.jpgalovic.daydream.model.util.Timer;
 
@@ -13,10 +16,14 @@ import java.util.ArrayList;
 public class HighScores extends State {
     // Object Data
     private ArrayList<ScoreDisplay> scoreDisplay;
+    private TexturedMeshObject backButton;
+    private TexturedMeshObject objectFindTheBlock;
+    private TexturedMeshObject objectBlock;
 
     // State Data
     private ScoreManager scoreManager;
-    private Timer exitTimer;
+
+    private boolean FLAG_EXIT;
 
     public HighScores(Context context) {
         super("STATE_HIGH_SCORES", context);
@@ -30,28 +37,38 @@ public class HighScores extends State {
         scoreDisplay.clear();
 
         for(int i = 0; i < 12; i++) {
-            scoreDisplay.add(new ScoreDisplay(context, scoreManager.getScore(i), 0.0f, -6.4f + (i * 1.4f), - 15.0f, 0.0f, 0.0f, 0.0f));
+            scoreDisplay.add(new ScoreDisplay(context, scoreManager.getScore(i), 0.0f, -6.4f + (i * 1.4f), - 17.0f, 0.0f, 0.0f, 0.0f));
         }
 
-        exitTimer = new Timer(20);
-        exitTimer.start();
+        FLAG_EXIT = false;
     }
 
     @Override
     public void init() {
+        backButton = new TexturedMeshObject("OBJ_BACK", false, Data.getMesh(context, R.array.OBJ_LABEL_BACK), Data.getTextures(context, R.array.OBJ_LABEL_BACK), 0.0f, -3.4f, -5.0f, 0.0f, 0.0f, 0.0f);
+        backButton.setScale(0.5f, 0.5f, 0.5f);
+
+        objectFindTheBlock = new TexturedMeshObject("OBJECT_FIND_THE_BLOCK_LABEL", false, Data.getMesh(context, R.array.OBJ_LABEL_FIND_THE_BLOCK), Data.getTextures(context, R.array.OBJ_LABEL_FIND_THE_BLOCK), 0.0f,  4.4f, -5.0f, 0.0f, 0.0f, 0.0f);
+
+        objectBlock = new TexturedMeshObject("OBJECT_BLOCK", false, Data.getMesh(context, R.array.OBJ_FTB_BLOCK), Data.getTextures(context, R.array.OBJ_FTB_BLOCK), 0.0f, 4.8f, -6.0f, 0.0f, 0.0f, 0.0f);
+        objectBlock.setScale(0.5f, 0.5f, 0.5f);
     }
 
     @Override
     public void input(float[] headView) {
-        // TODO: Process User Inputs.
+        if(backButton.isLookedAt(headView)) {
+            FLAG_EXIT = true;
+        }
     }
 
     @Override
     public State update() {
-        if(exitTimer.zero()) {
+        if(FLAG_EXIT == true) {
             connected.get(0).onDisplay();
             return connected.get(0);
         }
+
+        objectBlock.rotate(0.1f, 0.5f, -0.5f);
 
         return this;
     }
@@ -61,5 +78,9 @@ public class HighScores extends State {
         for(int i = 0; i < scoreDisplay.size(); i++) {
             scoreDisplay.get(i).render(perspective, view, objectProgram, objectModelViewProjectionParam);
         }
+        backButton.render(perspective, view, headView, objectProgram, objectModelViewProjectionParam);
+
+        objectBlock.render(perspective, view, 0, objectProgram, objectModelViewProjectionParam);
+        objectFindTheBlock.render(perspective, view, 0, objectProgram, objectModelViewProjectionParam);
     }
 }
